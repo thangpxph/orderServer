43193fab31cdd981a3ec8fdea93c10a2939c6601
phamthang <you@example.com>
2020-11-12 10:21:36 +0700

    addCategory

--- routes/category.js
+++ routes/category.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Category = require('../models/Category');
 
 router.get('/', async function (req, res) {
     res.render('category/index.hbs')
@@ -7,5 +8,21 @@ router.get('/', async function (req, res) {
 router.get('/addCategory', async function (req, res) {
     res.render('category/addCategory/addCategory.hbs')
 })
-
+router.post('/add_category', async function (req, res) {
+    const nameCategory = req.body.nameCategory;
+    let category = await Category.find({nameCategory: nameCategory}).lean();
+    if (category <=0){
+        const addCategory = new Category({
+            nameCategory: nameCategory
+        });
+        let status = await addCategory.save();
+        if (status) {
+            res.render('category/addCategory/addCategory.hbs', {status: 'block', data: 'Thêm bàn thành công'});
+        } else {
+            res.render('category/addCategory/addCategory.hbss', {status: 'block', data: 'Thêm bàn không thành công'})
+        }
+    } else {
+        res.render('category/addCategory/addCategory.hbs', {status: 'block', data: 'Thêm bàn không thành công, bàn đã tồn tại'})
+    }
+})
 module.exports = router;
